1568e35773ab8f6f1856734371d1f9a85c9d69c6
saad942 <ammarisaad343@gmail.com>
2024-10-15 00:15:22 +0100

    start home page

--- front-mld/src/App.js
+++ front-mld/src/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Routes} from 'react-router-dom';
-import Nav from './nav/Nav'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './nav/Nav';
+import Home from './home/Home';
+
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Router>
-
         <Routes>
-
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
